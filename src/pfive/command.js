c4067f1cfdb98f1df8d2114768b76e6a4d7e56cd
indmind <sattanationmail@gmail.com>
2017-12-16 18:43:09 +0700

    update command

--- src/pfive/command.js
+++ src/pfive/command.js
@@ -12,27 +12,36 @@ module.exports.run = () => {
         .description(description);
 
     program
-        .option('-off, --offline', 'Install from existing libraries');
+        .option('-v, --version', 'Show version', version)
+        .option("-o, --offline", "Offline installer");
 
     program
         .command("init")
+        .description("Create pfive.json file")
         .action(() => generator.init(process.cwd()))
 
     program
         .command("install")
         .alias("i")
+        .description("Install libraries from pfive.json")
         .action(() => generator.install(process.cwd(), program.offline))
 
     program
         .command("lib")
         .alias("l")
+        .description("Edit pfive.json libraries")
         .action(() => generator.addLib(process.cwd()))
 
     program
         .command("prune")
         .alias("p")
+        .description("Delete unused libreries")
         .action(() => generator.cleanUnusedLib(process.cwd()))
 
 
     program.parse(process.argv);
+
+    if (program.args.length < 1 ) {
+        console.log("try 'pfive -h' for help");
+    }
 }
