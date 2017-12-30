3acb8a32df6f002c115dfe376c0bc621447b2ef9
indmind <sattanationmail@gmail.com>
2017-12-30 17:17:10 +0700

    custom default js and css name

--- src/pfive/templator.js
+++ src/pfive/templator.js
@@ -1,21 +1,36 @@
-const cheerio = require('cheerio');
+const cheerio = require("cheerio");
 const path = require("path");
 const libData = require("../../data/libData.json");
-const beautify_html = require('js-beautify').html;
+const beautify_html = require("js-beautify").html;
 
-module.exports.compileHTML = (html, data)=> {
+module.exports.compileHTML = (html, data, usrJS, usrCSS, oldPfive = false) => {
     const $ = cheerio.load(html);
 
-    $("title").text((i, t) => t === '{PACKAGE_NAME}'? data.name: t);
+    $("title").text((i, t) => {
+        if (t === "{PACKAGE_NAME}" || (oldPfive && t === oldPfive.name)) {
+            return data.name;
+        }
+        return t;
+    });
 
-    for(lib of Object.keys(libData)){
+    if (!$("#p5-lib").length) {
+        $('<div id="p5-lib"></div>').appendTo("body");
+    }
+
+    for (let lib of Object.keys(libData)) {
         $(`#p5-lib #${lib.replace(/\./g, "-")}`).remove();
     }
 
-    for(lib of data.lib){
-        const script = `<script id="${lib.replace(/\./g, "-")}" src="${path.join("p5_lib", lib)}"></script>`;
+    if (usrJS) $("#main-script").attr("src", usrJS);
+    if (usrCSS) $("#main-style").attr("href", usrCSS);
+
+    for (let lib of data.lib) {
+        const script = `<script id="${lib.replace(
+            /\./g,
+            "-"
+        )}" src="${path.join("p5_lib", lib)}"></script>`;
         $("#p5-lib").append(script);
     }
 
-    return beautify_html($.html(), {max_preserve_newlines: 1});
-}
+    return beautify_html($.html(), { max_preserve_newlines: 1 });
+};
